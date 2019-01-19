8703d8152e0c82f0f01ef333694fe7749e43a6ed
gpx <1457495424@qq.com>
2019-01-19 16:11:56 +0800

    add some notes in 13_2.cpp

--- 13_2.cpp
+++ 13_2.cpp
@@ -5,12 +5,15 @@
 	> Created Time: 2019年01月19日 星期六 10时57分29秒
  ************************************************************************/
 // 0/1背包的优化
+// 优化了空间
 
 #include <iostream>
 using namespace std;
 #define MAX_N 1000000
 
-int dp[MAX_N + 5] = {0};
+int dp[MAX_N + 5] = {0};            
+//第i次求dp[j]时，此时dp[j]和dp[j-w[i]]表示的就是dp[i-1][j]和dp[i-1][j-w[i]]
+//我们便可用一维数组来代替之前的二维数组
 
 int main() {
     int n, total, w, v;
