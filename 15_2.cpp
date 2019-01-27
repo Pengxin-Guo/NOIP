5dc47c6665802bcef5f26bb93354b5f163140a85
gpx <1457495424@qq.com>
2019-01-27 19:05:53 +0800

    modify 15_2.cpp

--- 15_2.cpp
+++ 15_2.cpp
@@ -4,6 +4,7 @@
 	> Mail: 1457495424@qq.com
 	> Created Time: 2019年01月26日 星期六 21时02分31秒
  ************************************************************************/
+// 把多重背包当做0/1背包
 // 多重背包的二进制优化
 
 #include <iostream>
@@ -12,7 +13,7 @@ using namespace std;
 #define MAX_V 100000
 
 int dp[MAX_V + 5] = {0};
-int v[MAX_N + 5] = {0}, w[MAX_N + 5] = {0}, s[MAX_N + 5] = {0};
+int v[MAX_N + 5], w[MAX_N + 5], s[MAX_N + 5];
 int n, V;
 
 int main() {
@@ -22,8 +23,8 @@ int main() {
     }
     for (int i = 0; i < n; i++) {
         for (int k = 1; k <= s[i]; k <<= 1) {
-            int K = (s[i] - k + 1 < k ? s[i] - k + 1 : k);
-            int ww = w[i] * K, vv = v[i] * K;
+            int kk = (s[i] - k + 1 < k ? s[i] - k + 1 : k);
+            int ww = w[i] * kk, vv = v[i] * kk;
             for (int j = V; j >= ww; j--) {
                 dp[j] = max(dp[j], dp[j - ww] + vv);
             }
