ab44cf6380f79623e4f10db0c85cd1b3db59cf00
gpx <1457495424@qq.com>
2019-01-29 21:05:56 +0800

    add 11_3.cpp

---
+++ 11_3.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+	> File Name: 11_3.cpp
+	> Author: gpx
+	> Mail: 1457495424@qq.com
+	> Created Time: 2019年01月29日 星期二 20时42分48秒
+ ************************************************************************/
+// 多重背包的单调队列优化
+
+#include <iostream>
+using namespace std;
+#define MAX_N 100
+#define MAX_V 100000
+
+int dp[MAX_N + 5][MAX_V + 5] = {0};
+int v[MAX_N + 5], w[MAX_N + 5], s[MAX_N + 5];
+struct Data {
+    int val, j;
+} q[MAX_V + 5];
+int head, tail;
+int n, V;
+
+int main() {
+    cin >> V >> n;
+    for (int i = 1; i <= n; i++) cin >> w[i] >> v[i] >> s[i];
+    for (int i = 1; i <= n; i++) {
+        for (int r = 0; r < w[i]; r++) {
+            head = 0, tail = 0;
+            q[tail++] = {dp[i - 1][r], r};
+            dp[i][r] = dp[i - 1][r];
+            for (int j = r + w[i]; j <= V; j += w[i]) {
+                int val = dp[i - 1][j] - j / w[i] * v[i];
+                while (head < tail && val >= q[tail - 1].val) --tail;
+                q[tail++] = {val, j};
+                if ((j - q[head].j) / w[i] > s[i]) ++head;
+                dp[i][j] = q[head].val + j / w[i] * v[i];
+            }
+        }
+    }
+    cout << dp[n][V] << endl;
+    return 0;
+}
